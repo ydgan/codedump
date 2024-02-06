7c5142f47c72af610327a84b2a4351ef4e725621
valkyraja <134144614+ydgan@users.noreply.github.com>
2024-02-07 00:51:10 +1100

    Update preprocess.py

--- machine_learning/preprocess.py
+++ machine_learning/preprocess.py
@@ -16,12 +16,14 @@ for root, dirs, files in os.walk(r'E:\data\machine_learning\dataset'):
         mdata = pd.read_csv(os.path.join(root, file), index_col=0)
         mdata = mdata.replace(0, np.nan)
 
-        mdata['MidPrice'] = ((mdata['AskPrice1'] + mdata['BidPrice1']) / 2).pct_change()
+        mdata['MidPrice'] = ((mdata['AskPrice1'] + mdata['BidPrice1']) / 2)
+        mdata['MidPrice'] = np.log(mdata['MidPrice']).diff()
+        
         for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 'A']:
             #mdata['Wap%s'%i] = ((mdata['BidPrice%s'%i] * mdata['AskVolume%s'%i]) +
                                  #(mdata['AskPrice%s'%i] * mdata['BidVolume%s'%i])) /\
                                     #(mdata['AskVolume%s'%i] + mdata['BidVolume%s'%i])
-            #mdata['Wap%s'%i] = mdata['Wap%s'%i].pct_change()
+            #mdata['Wap%s'%i] = np.log(mdata['Wap%s'%i]).diff()
 
             '''
             mdata['AskPriceSpread%s'%i] = (mdata['AskPrice%s'%i] - mdata['MidPrice']) / mdata['MidPrice']
@@ -37,8 +39,8 @@ for root, dirs, files in os.walk(r'E:\data\machine_learning\dataset'):
             mdata['BidVolumeChange%s'%i] = mdata['BidVolume%s'%i].diff() / mdata['MidVolume']
             '''
 
-            mdata['AskPrice%s'%i] = mdata['AskPrice%s'%i].pct_change()
-            mdata['BidPrice%s'%i] = mdata['BidPrice%s'%i].pct_change()
+            mdata['AskPrice%s'%i] = np.log(mdata['AskPrice%s'%i]).diff()
+            mdata['BidPrice%s'%i] = np.log(mdata['BidPrice%s'%i]).diff()
 
         mdata.dropna(inplace=True)
         save_path = os.path.join(r'E:\data\machine_learning\preprocessed', root.split('\\')[-1])
